e05b5415448faeafced64d6a809f72ac3e2966e2
Makhdoom Shabir <MShabir@qa.com>
2020-10-11 19:05:18 +0100

    duplicated EC2 directory to allow two EC2 instances to be created

---
+++ terraform/EC2-2/variables.tf
@@ -0,0 +1,14 @@
+variable "ami-id" {
+  description = "AMI ID of ubuntu 18.04LTS eu-west-1"
+  default     = "ami-0823c236601fef765"
+}
+
+variable "instance-type" {
+  description = "Free tier EC2 Instance type"
+  default     = "t2.micro"
+}
+
+variable "pem-key" {
+  description = "Associated Key to SSH into the EC2 Instance"
+  default     = "crud solution"
+}
