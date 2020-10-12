29d9e10e90ecf364b7cc367157cea286adeb6913
Makhdoom Shabir <MShabir@qa.com>
2020-10-12 10:15:21 +0100

    made RDS password and username as env variables

---
+++ terraform/RDS/variables.tf
@@ -0,0 +1,9 @@
+variable "username" {
+  description = "The username for the DB master user"
+  type        = string
+}
+
+variable "password" {
+  description = "The password for the DB master user"
+  type        = string
+}
